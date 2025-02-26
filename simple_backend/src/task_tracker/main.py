adf5c4c3880675490c65779a6d3c978f02ada68b
Салахова Аделя <delyasalakhova@gmail.com>
2025-02-26 16:59:01 +0300

    первый вариант 2 задания с словарем для хранения задач

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -2,18 +2,34 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/tasks")
+tasks = {
+    1: {"task": "Купить хлеб", "done": True},
+    2: {"task": "ДЗ по ООП", "done": False},
+}
+
+@app.get("/tasks")  
 def get_tasks():
-    pass
+    return tasks  
 
-@app.post("/tasks")
-def create_task(task):
-    pass
+@app.post("/tasks")  
+def create_task(task: str, done: bool = False):
+    new_id = max(tasks.keys(), default=0) + 1  
+    tasks[new_id] = {"task": task, "done": done}  
+    return {"message": "Задача добавлена", "task_id": new_id, "task": tasks[new_id]}
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+@app.put("/tasks/{task_id}")  
+def update_task(task_id: int, task: str = None, done: bool = None):
+    if task_id in tasks:
+        if task is not None:
+            tasks[task_id]["task"] = task
+        if done is not None:
+            tasks[task_id]["done"] = done
+        return {"message": "Задача обновлена", "task": tasks[task_id]}
+    return {"message": "Задача не найдена"}
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    if task_id in tasks:
+        del tasks[task_id]
+        return {"message": "Задача удалена"}
+    return {"message": "Задача не найдена"}
